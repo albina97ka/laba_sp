69004e0ceb81fe6a1837e246471603dd21637210
albina97ka <122766344+albina97ka@users.noreply.github.com>
2023-09-22 14:11:53 +0300

    Add files via upload

--- Main2.java
+++ Main2.java
@@ -1,40 +1,63 @@
-import java.util.Random;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main2 {
-    public static void main(String[] args) {
-        int[] numbers = new int[10000];
-        Random random = new Random();
 
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = random.nextInt(10000);
-        }
-
-        int min = numbers[0];
-        int min_3 = numbers[0];
-        int min_7 = numbers[0];
-        int min_21 = numbers[0];
-
-        for (int number : numbers) {
-            if (number < min) {
-                min = number;
-            } else if (number % 3 == 0 && number < min_3) {
-                min_3 = number;
-            } else if (number % 7 == 0 && number < min_7) {
-                min_7 = number;
-            } else if (number % 21 == 0 && number < min_21) {
-                min_21 = number;
+    public static void sequentialCopy(String sourcePath, String destinationPath) {
+        try (FileReader reader = new FileReader(sourcePath);
+             FileWriter writer = new FileWriter(destinationPath)) {
+            int character;
+            while ((character = reader.read()) != -1) {
+                writer.write(character);
             }
+            System.out.println("Последовательное завершено успешно!");
+        } catch (IOException e) {
+            System.err.println("Произошла ошибка при последовательном копировании: " + e.getMessage());
         }
+    }
 
-        int R1 = min * min_21;
-        int R2 = min_3 * min_7;
+    public static void parallelCopy(String sourcePath, String destinationPath) {
+        Thread copyThread = new Thread(() -> {
+            try (FileReader reader = new FileReader(sourcePath);
+                 FileWriter writer = new FileWriter(destinationPath)) {
+                int character;
+                while ((character = reader.read()) != -1) {
+                    writer.write(character);
+                }
+                System.out.println("Параллельное завершено успешно!");
+            } catch (IOException e) {
+                System.err.println("Произошла ошибка при параллельном копировании: " + e.getMessage());
+            }
+        });
 
-        if (R1 > R2) {
-            System.out.println(R1);
-        } else if (R2 > R1) {
-            System.out.println(R2);
-        } else {
-            System.out.println("-1");
+        copyThread.start();
+        try {
+            copyThread.join();
+        } catch (InterruptedException e) {
+            System.err.println("Произошла ошибка при ожидании завершения параллельного потока копирования: " + e.getMessage());
         }
+    }
+
+    public static void main(String[] args) {
+        String sourceFile1 = "file1";
+        String destinationFile1 = "file2";
+        String sourceFile2 = "file1";
+        String destinationFile2 = "file2";
+
+        long startTime = System.currentTimeMillis();
+        sequentialCopy(sourceFile1, destinationFile1);
+        sequentialCopy(sourceFile2, destinationFile2);
+        long endTime = System.currentTimeMillis();
+        long sequentialTime = endTime - startTime;
+
+        startTime = System.currentTimeMillis();
+        parallelCopy(sourceFile1, destinationFile1);
+        parallelCopy(sourceFile2, destinationFile2);
+        endTime = System.currentTimeMillis();
+        long parallelTime = endTime - startTime;
+
+        System.out.println("Последовательное копирование: " + sequentialTime + " мс");
+        System.out.println("Параллельное копирование: " + parallelTime + " мс");
     }
 }
