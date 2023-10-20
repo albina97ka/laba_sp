fc9df529e2237acd8c70f33bdd1f3edd969665ed
albina97ka <122766344+albina97ka@users.noreply.github.com>
2023-10-20 12:27:13 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,13 +1,37 @@
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        int[] vector = new int[]{1, 2, 3, 4, 5};
-        int mul = 2;
+        String fileName = "file1"; // замените на путь к вашему файлу
 
-        for (int i = 0; i < vector.length; i++) {
-            vector[i] *= mul;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            Pattern pattern = Pattern.compile("X{1,}", Pattern.DOTALL);
+
+            StringBuilder text = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                text.append(line);
+            }
+
+            Matcher matcher = pattern.matcher(text);
+
+            int mathMax = 0;
+
+            while (matcher.find()) {
+                int math = matcher.group().length();
+                if (math > mathMax) {
+                    mathMax = math;
+                }
+            }
+
+            System.out.println("Длина самой длинной последовательности символов X: " + mathMax);
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка при работе с файлом.");
         }
-        System.out.println(Arrays.toString(vector));
     }
 }
